0110398660380a672a9e51e4e26d3ca82da4d4c0
olgarose <olgarose@users.noreply.github.com>
2018-09-06 16:05:00 -0700

    Serve static folder

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-//  res.send('<h1>Hello Express!</h1>');
   res.send({
     name: 'Olga',
     likes: [
@@ -23,4 +24,6 @@ app.get('/bad', (req, res) => {
   })
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
